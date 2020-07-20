88564c34df9bc724fb37b05c5da35a8f76fb6381
bayan-97 <bayanalalem0@gmail.com>
2020-07-20 20:31:48 +0300

    lab11

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ var firItemsindex;
 var midItemsindex;
 var secItemsindex;
 var  totalClicks=0;
+var userClicks=[];
+
+var itemsUniq=[];
+
+
+var itemofItemShown=[];
 function Items(path,name){
     this.path=path
     this.name=name
-  
+    this.uniq=true;
     this.userClicks=0;
     this.itemofItemShown=0;
     allitems.push(this);
@@ -38,31 +44,91 @@ itemsSection.addEventListener('click', itemClickHandler);
 
 
 function generateRandomImg() {
+    var itemsindex=[];
+   
+//    for (let i = 0; i < itemsindex.length; i++) {
+//     itemsindex.pop(itemsindex[i]);
+//     }
+   
     var firItems = document.getElementById('itemfir');
     var midItems = document.getElementById('itemmid');
     var secItems = document.getElementById('itemsec');
     firItemsindex= generateRandomnumber();
     midItemsindex=generateRandomnumber();
     secItemsindex=generateRandomnumber();
-    while(firItemsindex===midItemsindex  ||firItemsindex===secItemsindex ||midItemsindex===secItemsindex  ) {
-     
-        firItemsindex= generateRandomnumber();
-        midItemsindex=generateRandomnumber();
-        secItemsindex=generateRandomnumber();
+   
+    console.log(itemsUniq.includes(firItemsindex) , itemsUniq.includes(secItemsindex),itemsUniq.includes(midItemsindex));
+    // if(itemsUniq.includes(firItemsindex) || itemsUniq.includes(secItemsindex)||itemsUniq.includes(midItemsindex)) {
 
+       
+    //     generateRandomImg();
+    //     console.log("10");
+       
+    //      }
+    // checker(firItemsindex,secItemsindex,midItemsindex)
+    
+    
+    
+    while(firItemsindex===midItemsindex  ||firItemsindex===secItemsindex ||midItemsindex===secItemsindex  ) {
+         console.log(itemsUniq.includes(firItemsindex) , itemsUniq.includes(secItemsindex),itemsUniq.includes(midItemsindex));
+        
+         firItemsindex= generateRandomnumber();
+         midItemsindex=generateRandomnumber();
+         secItemsindex=generateRandomnumber();
+        //   checker(firItemsindex,secItemsindex,midItemsindex);
     }
+    // checker(firItemsindex,secItemsindex,midItemsindex);
+    console.log(itemsUniq.includes(firItemsindex) , itemsUniq.includes(secItemsindex),itemsUniq.includes(midItemsindex));
+
+//  if ( !allitems[ firItemsindex]) {
+     
+//  }
+
+
+  
+    // while(itemsUniq.includes(firItemsindex) || itemsUniq.includes(secItemsindex)||itemsUniq.includes(midItemsindex)) {
+
+       
+    //     firItemsindex= generateRandomnumber();
+    //     midItemsindex=generateRandomnumber();
+    //     secItemsindex=generateRandomnumber();
+       
+    //      }
+         
+
+     console.table( itemsindex);
+    allitems[firItemsindex].itemofItemShown += 1;
+    
+    allitems[midItemsindex].itemofItemShown += 1;
+
+    allitems[secItemsindex].itemofItemShown += 1;
+    
+   
+
+   
     var firPath = allitems[firItemsindex].path;
     var midPath = allitems[midItemsindex].path;
     var secPath = allitems[secItemsindex].path;
-
-    allitems[firItemsindex].itemofItemShown += 1;
-    allitems[midItemsindex].itemofItemShown += 1;
-    allitems[secItemsindex].itemofItemShown += 1;
-
-
     firItems.setAttribute('src',firPath);
-   midItems.setAttribute('src', midPath);
+    midItems.setAttribute('src', midPath);
    secItems.setAttribute('src', secPath);
+   itemsindex.push(firItemsindex);
+   itemsindex.push(midItemsindex);
+   itemsindex.push(secItemsindex);
+   
+   itemsUniq=[];
+   for (let i = 0; i < 3; i++) {
+       
+    itemsUniq.push(itemsindex[i]);
+    }
+    // console.table(itemsUniq);
+// allitems[firItemsindex].uniq=false;   
+// allitems[midItemsindex].uniq=false;
+// allitems[secItemsindex].uniq=false;
+
+  
+  
+  
     
 }
 
@@ -72,14 +138,16 @@ function  generateRandomnumber(){
 
 function  itemClickHandler() {
     if (totalClicks < 25 ){
+       
         var clickedElement = event.target;
         var clickedElementId = clickedElement.id;
-
+          
         if(clickedElementId === 'itemfir' || clickedElementId === 'itemmid'||clickedElementId === 'itemsec' ){
             totalClicks +=1;
 
             if(clickedElementId === 'itemfir'){
                 allitems[firItemsindex].userClicks +=1;
+
             }
 
             if(clickedElementId === 'itemmid'){
@@ -92,17 +160,20 @@ function  itemClickHandler() {
            
 
         }
-        generateRandomImg();
+
+      generateRandomImg();
+       
       
     }else{
+        arryClick() 
         generateUserMessage();
+        generateMyChart()
        itemsSection.removeEventListener('click', itemClickHandler);
 
     }
 
 }
    
-    
     function generateUserMessage(){
         var ulElement = document.getElementById('result-to-user');    
         
@@ -112,4 +183,114 @@ function  itemClickHandler() {
             ulElement.appendChild(listItem);
         }
     
+    }
+    function arryClick() {
+        for (let i3 = 0; i3 < allitems.length; i3++) {
+            userClicks.push(allitems[i3].userClicks);
+            itemofItemShown.push(allitems[i3].itemofItemShown)
+        }
+        
+    }
+
+    function generateMyChart(){
+
+        var ctx = document.getElementById('myChart1').getContext('2d');
+        var myChart1 = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels:namesitems ,
+            datasets: [{
+              label: '# of views',
+              data:itemofItemShown,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 177, 64, 0.2)',
+                'rgba(255, 180, 64, 0.2)',
+                'rgba(255, 175, 64, 0.2)',
+                'rgba(255, 165, 64, 0.2)',
+                'rgba(255, 160, 64, 0.2)',
+                'rgba(255, 170, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels:namesitems ,
+            datasets: [{
+              label: '# of Clicks',
+              data: userClicks,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 177, 64, 0.2)',
+                'rgba(255, 180, 64, 0.2)',
+                'rgba(255, 175, 64, 0.2)',
+                'rgba(255, 165, 64, 0.2)',
+                'rgba(255, 160, 64, 0.2)',
+                'rgba(255, 170, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+    }
+
+// var exam=[1,2,3];
+    function checker(it1,it2,it3) {
+        while(itemsUniq.includes(it1) || itemsUniq.includes(it2)||itemsUniq.includes(it3)) {
+            // firItemsindex+=1
+            // midItemsindex+=1
+            // secItemsindex+=1
+            generateRandomImg();
+       console.log("10");
+            
+        }
+        
     }
